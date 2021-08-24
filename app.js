85d9a92fe32596835c618d4accb99ef8fb4c224d
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-24 11:18:11 -0700

    "finished"

--- app.js
+++ app.js
@@ -31,3 +31,23 @@ let string = "packers";
 for (let i = 0; i < string.length; i++) {
   console.log(string[i]);
 }
+
+
+let counter = 5;
+let goal = 1;
+
+while(counter > 0) {
+  console.log("hello");
+  counter --;
+}
+
+let password = false;
+
+while ( password === false) {
+  if (prompt() === "hello kitty"){
+    console.log("User validated");
+    break;
+  }else{
+    console.log("wrong password")
+  }
+}
